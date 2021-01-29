8d93dc55e237e379f10642e43e60b2c905b3ab47
Daan Pothof <daanpothof@MacBook-Pro-van-Daan.local>
2021-01-29 11:58:04 +0100

    Working webpack!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import "./style.css";
 import printMe from "./print.js";
+import "./style.scss";
 
 function component() {
   const element = document.createElement("div");
@@ -8,6 +9,7 @@ function component() {
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
 
   btn.innerHTML = "Click me and check the console!";
   btn.onclick = printMe;
